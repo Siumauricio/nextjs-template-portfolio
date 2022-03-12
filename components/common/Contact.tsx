6129191d6074d71f4bb315a90070831d2317bf37
Siumauricio <Siumauricio@unitec.edu>
2022-03-11 22:30:01 -0600

    Email warning

--- components/common/Contact.tsx
+++ components/common/Contact.tsx
@@ -7,11 +7,11 @@ export const Contact = () => {
          <FadeIn>
             <div className="pb-14 flex  items-center flex-col pt-5 border-b border-base-300" id="Contact">
                <h2 className="sm:text-3xl text-2xl font-bold pb-7 ">Contact</h2>
-               <form action="mailto:Siumauricio@hotmail.com">
-                  <button className="btn btn-primary" type="submit">
-                     Send Email
-                  </button>
-               </form>
+               <a href="https://mailto:Siumauricio@hotmail.com" className="btn btn-primary">
+                  Send Email
+               </a>
+               {/* <button className="" type="submit">
+                  </button> */}
             </div>
          </FadeIn>
       </section>
