0eac44a0a8d73e7576c95fdf2d2ffd900c8f1975
Siumauricio <Siumauricio@unitec.edu>
2022-03-11 22:30:56 -0600

    warning email fix

--- components/common/Contact.tsx
+++ components/common/Contact.tsx
@@ -7,7 +7,7 @@ export const Contact = () => {
          <FadeIn>
             <div className="pb-14 flex  items-center flex-col pt-5 border-b border-base-300" id="Contact">
                <h2 className="sm:text-3xl text-2xl font-bold pb-7 ">Contact</h2>
-               <a href="https://mailto:Siumauricio@hotmail.com" className="btn btn-primary">
+               <a href="mailto:Siumauricio@hotmail.com" className="btn btn-primary">
                   Send Email
                </a>
                {/* <button className="" type="submit">
